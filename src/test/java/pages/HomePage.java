5abbb0404801ceda4a71a45d7cbb56f5c4a0e4c5
juliaumar <57296716+juliaumar@users.noreply.github.com>
2023-01-27 18:28:16 -0500

    ASTF-3 Parallax Sectio

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -19,7 +19,29 @@ public class HomePage extends BasePage {
     @FindBy(xpath = "//a[text()='Join Now']")
     public WebElement joinNowBtn;
 
+    //julia Parallax Section
+    @FindBy(xpath = "//h2[normalize-space()='A bright career iswaiting for you...']")
+    public WebElement header1;
 
+    //julia Parallax Section
+    @FindBy(xpath = "//h2[normalize-space()='Think Big.Achieve Bigger.']")
+    public WebElement header2;
+
+    //julia Parallax Section
+    @FindBy(xpath = "//li[1]/div[3]/div[1]/div[1]/div[1]/div[1]")
+    public WebElement text1;
+
+    //julia Parallax Section
+    @FindBy(xpath = "//ul[1]/li[2]/div[3]/div[1]/div[1]/div[1]/div[1]")
+    public WebElement text2;
+
+    //julia Parallax Section
+    @FindBy(xpath = "//li[1]/div[4]/div[1]/div[1]/div[1]/a[1]")
+    public WebElement readMoreBtn;
+
+    //julia Parallax Section
+    @FindBy(xpath = "//ul[1]/li[2]/div[4]/div[1]/div[1]/div[1]/a[1]")
+    public WebElement readMoreBtn2;
 
 
 
--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import base.BaseTest;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 
 import org.openqa.selenium.WebElement;
@@ -10,6 +12,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.CommonPage;
 import pages.HomePage;
+import utils.ExtentManager;
+import utils.Screenshot;
+import utils.SeleniumUtils;
 
 
 public class HomeTest extends BaseTest {
@@ -32,6 +37,29 @@ public class HomeTest extends BaseTest {
     @Test
     public void test02() {
         Assert.assertTrue(homePage.joinNowBtn.isDisplayed());
+
+    }
+    @Test(testName = "Parallax Section")
+    public void test03(){
+        homePage.isDisplayed(homePage.header1);
+        homePage.isDisplayed(homePage.text1);
+        Actions at = new Actions(getDriver());
+        at.sendKeys(Keys.PAGE_UP).build().perform();
+        homePage.click(homePage.readMoreBtn);
+        Assert.assertEquals( getDriver().getTitle(), "Advance Systems - Services");
+        Screenshot.takeScreenshot(getDriver());
+        getDriver().navigate().back();
+        homePage.waitForElementClickability(homePage.header2);
+        homePage.isDisplayed(homePage.header2);
+        Screenshot.takeScreenshot(getDriver());
+        homePage.isDisplayed(homePage.text2);
+        at.sendKeys(Keys.PAGE_UP).build().perform();
+        homePage.click(homePage.readMoreBtn2);
+
+
+
+
+
     }
 
 
