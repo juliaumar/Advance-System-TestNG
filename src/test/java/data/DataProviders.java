785cd2f9916742a9109a2e05739bf09266b86d7d
Nipaporn-Chen <amp6722.ja@gmail.com>
2023-01-29 08:18:55 -0500

    modified DataProviders

--- src/test/java/data/DataProviders.java
+++ src/test/java/data/DataProviders.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 public class DataProviders {
     @DataProvider(name = "quickLinks")
     public static Object[][] quickLinks(){
-        Object[][] data = new Object[5][1];
+        Object[][] data = new Object[5][2];
         data[0][0] = "Home";
         data[1][0] = "About Us";
         data[2][0] = "Services";
