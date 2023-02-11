2ffd06157c6347620f59bd5a50c880eb8eac9708
NikolayGarmaev <nikolaygarmaev91@gmail.com>
2023-02-11 10:26:59 -0800

    ASTF-29: About Us page: Why Us

--- src/test/java/tests/AboutUsTest.java
+++ src/test/java/tests/AboutUsTest.java
@@ -3,11 +3,16 @@ package tests;
 import base.BaseTest;
 
 import data.DataProviders;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.AboutUsPage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AboutUsTest extends BaseTest {
@@ -23,4 +28,19 @@ public class AboutUsTest extends BaseTest {
         aboutUsPage.aboutLink.click();
         aboutUsPage.isDisplayed(aboutUsPage.expertsHeader);
         aboutUsPage.verifyExpertsWithPosition(position, url, name, title, description);
-    }}
+    }
+
+    @Test(testName = "ASTF-29: About Us page: Why Us")
+    public void testASTF29() {
+        aboutUsPage.aboutLink.click();
+
+        int i;
+        for (i = 0; i < aboutUsPage.whyChooseUsSection.size(); i++) {
+            aboutUsPage.moveElementToTheMiddleOfView(aboutUsPage.whyChooseUsSection.get(i));
+            aboutUsPage.highlightElement(aboutUsPage.whyChooseUsSection.get(i).findElement(By.cssSelector("h3")));
+            Assert.assertEquals(aboutUsPage.whyChooseUsSection.get(i).findElement(By.cssSelector("h3")).getText(), aboutUsPage.whyChooseUsSectionDesc[i]);
+
+        }
+        Assert.assertEquals(aboutUsPage.whyChooseUsSection.size(), 4);
+    }
+}
